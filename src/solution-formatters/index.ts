ae0acbfbdd4906f440ceb632bcabfa7f200dfbaf
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-24 14:47:53 -0400

    Order solution formatters

--- src/solution-formatters/index.ts
+++ src/solution-formatters/index.ts
@@ -3,13 +3,15 @@ import path from 'path';
 
 import { CompletedKata, CompletedKataFormatter } from '../types';
 
+const ORDER = ['jsonify.js', 'jsonify.ts']
+
 export default async function formatKatas(katas: Record<string, CompletedKata>, output: string) {
   const formattersDirectory = path.dirname(__filename);
-  const requestedSolutionFormatters = (process.argv.find(arg => arg.startsWith('--formatter=')) ?? '=')
+  const requestedSolutionFormatters = (process.argv.find(arg => arg.startsWith('--formatters=')) ?? '=')
     .split('=')[1]
     .split(',')
     .filter(Boolean);
-  for (const filename of await fs.promises.readdir(formattersDirectory)) {
+  for (const filename of (await fs.promises.readdir(formattersDirectory)).sort((a, b) => ORDER.indexOf(b) - ORDER.indexOf(a))) {
     if (filename.startsWith('index')) continue;
 
     const { DIRECTORY_NAME, default: format }: { DIRECTORY_NAME: string; default: CompletedKataFormatter } =
@@ -17,9 +19,11 @@ export default async function formatKatas(katas: Record<string, CompletedKata>, 
     if (requestedSolutionFormatters.length && !requestedSolutionFormatters.includes(DIRECTORY_NAME)) continue;
 
     const formattedOutputDirectory = path.join(output, DIRECTORY_NAME);
+    console.log('Starting', filename.split('.')[0], '...');
     if (fs.existsSync(formattedOutputDirectory)) await fs.promises.rm(formattedOutputDirectory, { recursive: true });
     await fs.promises.mkdir(formattedOutputDirectory);
 
     await format(katas, formattedOutputDirectory);
+    console.log('Finished', filename.split('.')[0]);
   }
 }

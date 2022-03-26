fdc40500ab7c7636138cda17937454514187ce6d
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-26 16:03:34 -0400

    Fix daily-files commit order

--- src/solution-formatters/daily-files.ts
+++ src/solution-formatters/daily-files.ts
@@ -1,6 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import { generateCommentLine, getKataURL, getLanguageExtension, setEnvironmentVariable } from '../helpers';
+import {
+  generateCommentLine,
+  getKataURL,
+  getLanguageExtension,
+  getLanguageName,
+  setEnvironmentVariable,
+} from '../helpers';
 import { FORMATTERS__DISABLE_GIT, FORMATTER__DAILY_FILES__COMMIT_PER_KATA } from '../constants';
 
 import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
@@ -13,12 +19,19 @@ const formatDay = (date: Date) =>
     .toString()
     .padStart(2, '0')}`;
 
+const getSolutionLines = (solution: Solution, kata: CompletedKata) => {
+  return [
+    ...[kata.rank, kata.title, getKataURL(kata)].map(generateCommentLine.bind(null, solution.language)),
+    solution.content,
+  ];
+};
+
 const dailyFiles: CompletedKataFormatter = async function dailyFiles(katas, directory) {
   const git = simpleGit({ baseDir: directory });
   if (!FORMATTERS__DISABLE_GIT) await git.init();
 
   let done = 0;
-  let total = 0
+  let total = 0;
 
   const solutionKatas = new Map<number, CompletedKata>();
   const dates: Record<string, Solution[]> = {};
@@ -32,56 +45,74 @@ const dailyFiles: CompletedKataFormatter = async function dailyFiles(katas, dire
     }
   }
 
+  const sortAndAddKatas = (solutions: Solution[]) =>
+    solutions.sort((a, b) => a.when - b.when).map(solution => ({ solution, kata: solutionKatas.get(solution.when)! }));
+
+  const getFilenameAndPath = (date: string, language: string) => {
+    const filename = date + '.' + getLanguageExtension(language);
+    return { filename, filepath: path.join(directory, filename) };
+  };
+
   for (const [date, solutions] of Object.entries(dates).sort((a, b) => a[0].localeCompare(b[0]))) {
-    const languageSolutions = solutions.reduce((languages, solution) => {
-      return {
-        ...languages,
-        [solution.language]: [...(languages[solution.language] ?? []), solution],
-      };
-    }, {} as Record<string, Solution[]>);
-    for (const [language, solutions] of Object.entries(languageSolutions)) {
-      const filename = date + '.' + getLanguageExtension(language);
-      const filepath = path.join(directory, filename);
-
-      let solutionLines = [];
-      const sortedWithKatas = solutions
-        .sort((a, b) => a.when - b.when)
-        .map(solution => ({ solution, kata: solutionKatas.get(solution.when)! }));
-      for (const { solution, kata } of sortedWithKatas) {
+    if (FORMATTER__DAILY_FILES__COMMIT_PER_KATA) {
+      for (const { solution, kata } of sortAndAddKatas(solutions)) {
         process.stdout.write(`${((done++ / total) * 100).toFixed(2)}%       \r`);
-
-        solutionLines.push(
-          [
-            ...[kata.rank, kata.title, getKataURL(kata)].map(generateCommentLine.bind(null, language)),
-            solution.content,
-          ].join('\n'),
-        );
-        if (FORMATTERS__DISABLE_GIT || !FORMATTER__DAILY_FILES__COMMIT_PER_KATA) continue;
-
-        await fs.promises.writeFile(filepath, solutionLines.join('\n\n\n'));
+        const { filename, filepath } = getFilenameAndPath(date, solution.language);
+        const lines = getSolutionLines(solution, kata);
+        if (!fs.existsSync(filepath)) {
+          await fs.promises.writeFile(filepath, lines.join('\n') + '\n');
+        } else {
+          await fs.promises.appendFile(filepath, '\n\n\n' + lines.join('\n') + '\n');
+        }
 
         const writeTime = new Date(solution.when).toISOString();
 
         await setEnvironmentVariable('GIT_COMMITTER_DATE', writeTime, () =>
-          git.add(filename).commit(`Add "${kata.title}" challenge`, [], {
+          git.add(filename).commit(`Add ${date} "${kata.title}" ${getLanguageName(solution.language)} solution`, [], {
             '--date': writeTime,
           }),
         );
       }
-      await fs.promises.writeFile(filepath, solutionLines.join('\n\n\n'));
+    } else {
+      const languageSolutions = solutions.reduce((languages, solution) => {
+        return {
+          ...languages,
+          [solution.language]: [...(languages[solution.language] ?? []), solution],
+        };
+      }, {} as Record<string, Solution[]>);
+      for (const [language, solutions] of Object.entries(languageSolutions).sort(
+        (a, b) => Math.max(...a[1].map(s => s.when)) - Math.max(...b[1].map(s => s.when)),
+      )) {
+        const { filename, filepath } = getFilenameAndPath(date, language);
 
-      if (FORMATTERS__DISABLE_GIT || FORMATTER__DAILY_FILES__COMMIT_PER_KATA) continue;
+        let solutionLines = [];
+        const sortedWithKatas = sortAndAddKatas(solutions);
+        for (const { solution, kata } of sortedWithKatas) {
+          process.stdout.write(`${((done++ / total) * 100).toFixed(2)}%       \r`);
 
-      const writeTime = new Date(sortedWithKatas.at(-1)!.solution.when).toISOString();
-      await setEnvironmentVariable('GIT_COMMITTER_DATE', writeTime, () =>
-        git.add(filename).commit(`Add ${date} challenge${sortedWithKatas.length === 1 ? '' : 's'}`, [], {
-          '--date': writeTime,
-        }),
-      );
+          solutionLines.push(getSolutionLines(solution, kata));
+        }
+        await fs.promises.writeFile(filepath, solutionLines.join('\n\n\n'));
+
+        if (FORMATTERS__DISABLE_GIT) continue;
+
+        const writeTime = new Date(sortedWithKatas.at(-1)!.solution.when).toISOString();
+        await setEnvironmentVariable('GIT_COMMITTER_DATE', writeTime, () =>
+          git
+            .add(filename)
+            .commit(
+              `Add ${date} ${getLanguageName(language)} challenge${sortedWithKatas.length === 1 ? '' : 's'}`,
+              [],
+              {
+                '--date': writeTime,
+              },
+            ),
+        );
+      }
     }
   }
 
-  console.log()
+  console.log();
 };
 
 export const DIRECTORY_NAME = 'daily-files';

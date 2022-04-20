f0503e8aed6772f081a63d37ce93340a9efbf547
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-20 15:32:00 -0400

    Add caching to solution downloading

--- src/download-solutions.ts
+++ src/download-solutions.ts
@@ -9,16 +9,15 @@ import { JSDOM } from 'jsdom';
 import formatKatas from './solution-formatters';
 import { fetchKataLanguageInfo, parseKataLanguageInfo } from './helpers';
 
-
-async function getKataLanguageInfo(slug: string, language: string) {
-	let html;
-	const cachePath = path.join('cache', `${slug}-${language}.html`)
-	if (!fs.existsSync(cachePath)) {
-		html = await fetchKataLanguageInfo(slug, language);
-		await fs.promises.writeFile(cachePath, html);
-	} else {
-		html = (await fs.promises.readFile(cachePath)).toString()
-	}
+async function getKataLanguageInfo(slug: string, language: string, cache: boolean = true) {
+  let html;
+  const cachePath = path.join('cache', `${slug}-${language}.html`);
+  if (!cache || !fs.existsSync(cachePath)) {
+    html = await fetchKataLanguageInfo(slug, language);
+    await fs.promises.writeFile(cachePath, html);
+  } else {
+    html = (await fs.promises.readFile(cachePath)).toString();
+  }
 
   return parseKataLanguageInfo(html);
 }
@@ -33,13 +32,17 @@ function parseHTMLSolutions(html: string): Record<string, CompletedKata> {
         slug: anchor.href.split('/').at(-1)!,
         title: anchor.textContent!,
         rank: anchor.parentNode?.children[0].textContent!,
-        solutions: Array.from(rawSolutions.querySelectorAll('.markdown')).map(div => ({ div, datetime: div.nextElementSibling!.querySelector('time-ago')!.getAttribute('datetime')! })).filter(({ datetime }) => !IGNORE_SOLUTIONS.has(datetime)).map(({ div, datetime }) => {
-          return {
-            content: div.textContent!,
-            language: div.querySelector('code')?.dataset.language!,
-            when: new Date(datetime)!.getTime(),
-          };
-        }).sort((a, b) => a.when - b.when),
+        solutions: Array.from(rawSolutions.querySelectorAll('.markdown'))
+          .map(div => ({ div, datetime: div.nextElementSibling!.querySelector('time-ago')!.getAttribute('datetime')! }))
+          .filter(({ datetime }) => !IGNORE_SOLUTIONS.has(datetime))
+          .map(({ div, datetime }) => {
+            return {
+              content: div.textContent!,
+              language: div.querySelector('code')?.dataset.language!,
+              when: new Date(datetime)!.getTime(),
+            };
+          })
+          .sort((a, b) => a.when - b.when),
       };
     })
     .reduce(
@@ -51,28 +54,55 @@ function parseHTMLSolutions(html: string): Record<string, CompletedKata> {
     );
 }
 
+const fetchFirstPage = async (cache: boolean) => {
+  const cachePath = path.join('cache', `page-${0}.html`);
 
-(async () => {
-  console.log('Downloading solutions...');
-  const response = await fetch(`https://www.codewars.com/users/${USER_NAME}/completed_solutions`, {
-    headers: {
-      'User-Agent': USER_AGENT,
-      Cookie: 'remember_user_token=' + REMEMBER_USER_TOKEN,
-    },
-  });
-  const katas = parseHTMLSolutions(await response.text());
+  let html;
+  if (!fs.existsSync(cachePath)) {
+    html = await fetch(`https://www.codewars.com/users/${USER_NAME}/completed_solutions`, {
+      headers: {
+        'User-Agent': USER_AGENT,
+        Cookie: 'remember_user_token=' + REMEMBER_USER_TOKEN,
+      },
+    }).then((response: any) => response.text());
+    await fs.promises.writeFile(cachePath, html);
+  } else {
+    html = (await fs.promises.readFile(cachePath)).toString();
+  }
 
-  let page = 1;
-  while (true) {
-    process.stdout.write(`Page #${page.toString().padStart(2, '0')} \r`);
-    const response = await fetch(`https://www.codewars.com/users/${USER_NAME}/completed_solutions?page=${page++}`, {
+  return parseHTMLSolutions(html);
+};
+
+const fetchPage = async (page: number, cache: boolean) => {
+  const cachePath = path.join('cache', `page-${page}.html`);
+
+  let html;
+  if (!fs.existsSync(cachePath)) {
+    html = await fetch(`https://www.codewars.com/users/${USER_NAME}/completed_solutions?page=${page++}`, {
       headers: {
         'User-Agent': USER_AGENT,
         'x-requested-with': 'XMLHttpRequest',
         Cookie: 'remember_user_token=' + REMEMBER_USER_TOKEN,
       },
-    });
-    const newRows = parseHTMLSolutions(await response.text());
+    }).then((response: any) => response.text());
+    await fs.promises.writeFile(cachePath, html);
+  } else {
+    html = (await fs.promises.readFile(cachePath)).toString();
+  }
+
+  return parseHTMLSolutions(html);
+};
+
+(async () => {
+  const CACHE_PAGES = process.argv.includes('--cache-pages');
+  console.log('Downloading solutions...');
+
+  const katas = await fetchFirstPage(CACHE_PAGES);
+
+  let page = 1;
+  while (true) {
+    process.stdout.write(`Page #${page.toString().padStart(2, '0')} \r`);
+    const newRows = await fetchPage(page++, CACHE_PAGES);
     Object.assign(katas, newRows);
     const length = Object.keys(newRows).length;
     if (!length) break;
@@ -86,20 +116,22 @@ function parseHTMLSolutions(html: string): Record<string, CompletedKata> {
   }
   let current = 0;
   for (const kata of Object.values(katas)) {
-    kata.info = {}
+    kata.info = {};
     for (const solution of kata.solutions) {
-      process.stdout.write(
-        `${((current++ / total) * 100).toFixed(2)}%      \r`,
+      process.stdout.write(`${((current++ / total) * 100).toFixed(2)}%      \r`);
+      const { description, testCode, vote, voteID, csrfToken } = await getKataLanguageInfo(
+        kata.slug,
+        solution.language,
       );
-      const { description, testCode, vote, voteID, csrfToken } = await getKataLanguageInfo(kata.slug, solution.language);
-      kata.info[solution.language] = { description, testCode }
+      kata.info[solution.language] = { description, testCode };
       kata.vote = vote;
       if (voteID) kata.voteID = voteID;
-      kata.csrfToken = csrfToken
+      kata.csrfToken = csrfToken;
     }
   }
 
-  console.log()
+  console.log();
 
+  process.argv.unshift('--formatters=json');
   return formatKatas(katas, './solutions_output');
 })().catch(console.error);

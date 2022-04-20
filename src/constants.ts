773dc3dfc7825bdbf805c1650c3ff8cc04d01712
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-20 14:27:18 -0400

    Update constant types

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-export const USER_AGENT = process.env.USER_AGENT;
-export const REMEMBER_USER_TOKEN = process.env.REMEMBER_USER_TOKEN;
+export const USER_AGENT = process.env.USER_AGENT!;
+export const REMEMBER_USER_TOKEN = process.env.REMEMBER_USER_TOKEN!;
 export const USER_NAME = process.env.USER_NAME!;
 export const EMAIL = process.env.EMAIL!;
 export const PASSWORD = process.env.PASSWORD!;
